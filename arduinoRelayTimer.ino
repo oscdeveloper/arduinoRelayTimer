37464e53cbacd2dff9bd9a7c5d412e7db6e84f54
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2019-09-02 02:04:59 +0200

    Add rotary encoder lib

--- arduinoRelayTimer.ino
+++ arduinoRelayTimer.ino
@@ -1,109 +1,65 @@
 #include <Wire.h> 
+#include <SimpleRotary.h>
 #include <LiquidCrystal_I2C.h>
 #include "RTClib.h"
 
+#define rotaryCCW 5
+#define rotaryCW 6
+#define rotaryBtn 7
+
 LiquidCrystal_I2C lcd(0x27,20,4);
 RTC_DS1307 rtc;
+ 
+// Pin A, Pin B, Button Pin
+SimpleRotary rotary(rotaryCW, rotaryCCW, rotaryBtn);
+byte rotaryState;
+byte rotaryBtnState;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
 DateTime now;
 
-unsigned int pinRelay = 11;
-
-bool relay = false;
-
-unsigned long time1;
-
-unsigned int counter = 0;
-
-#define outputA 2
-#define outputB 3
-#define knobSwitch 8
-//int counter = 0; 
-int aState;
-int aLastState;  
-
 void setup() {
-
-  pinMode (outputA,INPUT_PULLUP);
-  pinMode (outputB,INPUT_PULLUP);
-  
-  aLastState = digitalRead(outputA);  
  
   lcd.init();
   lcd.backlight();
-     
- 
 
-//  pinMode (pinRelay, OUTPUT);
-//  
-//  digitalWrite(pinRelay, LOW);
-//  // LOW state activate relay / switched on
-//  delay(5000);
-//
-//  time1 = millis();
+  rotary.setTrigger(LOW);
+  
   
   Serial.begin(9600);  
-  
 }
 
 void loop() {
-  
-  
-  
+ 
+  // 0 = not turning, 1 = CW, 2 = CCW
+  rotaryState = rotary.rotate();
 
-  aState = digitalRead(outputA);
+  if ( rotaryState == 1 ) {
+    Serial.println("CW");
+  }
 
-     
-   if (aState != aLastState){     
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-     if (digitalRead(outputB) != aState) { 
-       counter ++;
-     Serial.println("GORAaaaaaaaaaaaaaaaa");
-     displayMainScreen();
-     } else {
-       counter --;
-     Serial.println("DOL");
-     }
-     delay(100);
-   } 
-   aLastState = aState;
+  if ( rotaryState == 2 ) {
+    Serial.println("CCW");
+  }
+
+  rotaryBtnState = rotary.pushType(700);
+
+  if ( rotaryBtnState == 1 ) {
+    Serial.println(String("Pressed: ") + "Short");
+    displayMainScreen();
+  }
+
+  if ( rotaryBtnState == 2 ) {
+    Serial.println(String("Pressed: ") + "Long");
+  }
 //
 //  displayMainScreen();
    
 //  delay(10);
 
-//  Serial.print(counter);
-//  Serial.print("\t");
-
-//
-//if ( counter < 120 ) {
-//  
-//  digitalWrite(pinRelay, HIGH); // turn off
-//  delay(10000);
-//  
-//  digitalWrite(pinRelay, LOW); // turn on
-//  delay(5000);
-//  
-//  counter++;
-//
-//} else {
-//  digitalWrite(pinRelay, HIGH); // turn off
-//  delay(60000);
-//}
 
 
 
-//  if ( counter == 1 && ( ( millis() - time1 ) > 1500 ) ) {      
-//    relay1 = !relay1;
-//    digitalWrite(pinRelay1, relay1);
-//    counter = 0;      
-//  }
-
-//  Serial.print(relay1);
-//  Serial.print("\t");
-//  Serial.println(relay2);
 
 }
 
