a5a0ad33b1e3014bcf0d840236716f4d303e8609
Arkadiusz Krakiewicz <biuro@oscdeveloper.pl>
2019-09-03 23:16:19 +0200

    Update Settings menu

--- arduinoRelayTimer.ino
+++ arduinoRelayTimer.ino
@@ -1,3 +1,4 @@
+#include <EEPROM.h>
 #include <Wire.h> 
 #include <SimpleRotary.h>
 #include <LiquidCrystal_I2C.h>
@@ -14,6 +15,9 @@ RTC_DS1307 rtc;
 SimpleRotary rotary(rotaryCW, rotaryCCW, rotaryBtn);
 byte rotaryState;
 byte rotaryBtnState;
+byte rotaryBtnLongState;
+
+unsigned short eepromWorkingModeAdr = 0;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 DateTime now;
@@ -23,7 +27,7 @@ String settingsMenu[] = {"Clock Date/Time", "Working Mode", "Working Hours", "In
 //byte settingsMenuItemsSize = sizeof(settingsMenuItems[1]) / sizeof(settingsMenuItems[1][0]);
 
 short screenLevel = 1;
-bool drawScreenOnce = true;
+bool screenExit = false;
 unsigned short menuStart = 0;
 unsigned short menuEnd = 2;
 unsigned short indicatorRow = 1;
@@ -40,18 +44,15 @@ void printIndicator(String text = "") {
 }
 
 void drawSettingsMenu() {
-  if ( drawScreenOnce ) {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("SETTINGS");
-    printIndicator();
-    short row=1;
-    for (short menuItem=menuStart; menuItem<=menuEnd; menuItem++) {
-      lcd.setCursor(1,row);
-      lcd.print(settingsMenu[menuItem]);
-      row++;
-    }
-    drawScreenOnce = false;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("SETTINGS");
+  printIndicator();
+  short row=1;
+  for (short menuItem=menuStart; menuItem<=menuEnd; menuItem++) {
+    lcd.setCursor(1,row);
+    lcd.print(settingsMenu[menuItem]);
+    row++;
   }
 }
 
@@ -59,52 +60,60 @@ void runMenuSettings() {
 
   drawSettingsMenu();
 
-  if ( rotaryState == 1 ) { // CW
-    if (indicatorRow >= indicatorRowMax) {
-      if (menuStart == 3) {   
-        indicatorRow = indicatorRowMax;
-        printIndicator();
+  while(!screenExit) {
+
+    rotaryBtnState = rotary.push();
+    rotaryState = rotary.rotate();
+
+    if ( rotaryState == 1 ) { // CW
+      if (indicatorRow >= indicatorRowMax) {
+        if (menuStart == 3) {   
+          indicatorRow = indicatorRowMax;
+          printIndicator();
+        } else {
+          menuStart = 3;
+          menuEnd = 4;
+          indicatorRow=1;
+          indicatorRowMax=2;  
+          drawSettingsMenu();
+        }  
       } else {
-        drawScreenOnce = true;
-        menuStart = 3;
-        menuEnd = 4;
-        indicatorRow=1;
-        indicatorRowMax=2;  
-      }  
-    } else {
-      printIndicator(" ");
-      indicatorRow++;
-      printIndicator();
-    }
+        printIndicator(" ");
+        indicatorRow++;
+        printIndicator();
+      }
+      
+    } else if ( rotaryState == 2 ) { // CCW
+      if (indicatorRow <= 1) {
+        if (menuStart == 0) { 
+          indicatorRow = 1;
+          printIndicator();
+        } else {
+          menuStart = 0;
+          menuEnd = 2;
+          indicatorRow=3;
+          indicatorRowMax=3;
+          drawSettingsMenu();
+        }
+      } else {
+        printIndicator(" ");
+        indicatorRow--;
+        printIndicator();    
+      }
+    } 
     
-  } else if ( rotaryState == 2 ) { // CCW
-    if (indicatorRow <= 1) {
-      if (menuStart == 0) { 
-        indicatorRow = 1;
-        printIndicator();
-      } else {
-        drawScreenOnce = true;
-        menuStart = 0;
-        menuEnd = 2;
-        indicatorRow=3;
-        indicatorRowMax=3;
+    if ( rotaryBtnState == 1 ) {
+      if (menuStart == 0) {
+        switch (indicatorRow) {
+          case 1:
+            screenLevel = 0; // home
+            break;
+          case 2:
+            screenLevel = 2; // working type
+            break;
+        }
       }
-    } else {
-      printIndicator(" ");
-      indicatorRow--;
-      printIndicator();    
-    }
-  } else if ( rotaryBtnState == 1 ) {
-    drawScreenOnce = true;
-    if (menuStart == 0) {
-      switch (indicatorRow) {
-        case 1:
-          screenLevel = 0;
-          break;
-        case 2:
-          screenLevel = 2;
-          break;
-      }
+      screenExit = true;
     }
   }
 }
@@ -112,51 +121,83 @@ void runMenuSettings() {
 void runHomeScreen() {
   lcd.clear();
   now = rtc.now();
-  lcd.setCursor(6,0);
-  lcd.print(
-    formatDateNumber(now.hour())
-    + String(":")
-    + formatDateNumber(now.minute())
-    + String(":")
-    + formatDateNumber(now.second()));
-  lcd.setCursor(0,1);
-  lcd.print(
-    daysOfTheWeek[now.dayOfTheWeek()]
-    + String(", ")
-    + now.year()
-    + String("/")
-    + formatDateNumber(now.month())
-    + String("/")
-    + formatDateNumber(now.day()));
-  lcd.setCursor(3,2);
-  lcd.print("WORKING HOURS");
+  byte workingModeValue = EEPROM.read(eepromWorkingModeAdr);
+  if (workingModeValue != 0 && workingModeValue != 1) workingModeValue = 0;
+  
+  while(!screenExit) {
+
+    rotaryBtnState = rotary.push();
+    rotaryState = rotary.rotate();
+    
+    lcd.setCursor(6,0);
+    lcd.print(
+      formatDateNumber(now.hour())
+      + String(":")
+      + formatDateNumber(now.minute())
+      + String(":")
+      + formatDateNumber(now.second()));
+    lcd.setCursor(0,1);
+    lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    lcd.setCursor(10,1);
+    lcd.print(
+      now.year()
+      + String("/")
+      + formatDateNumber(now.month())
+      + String("/")
+      + formatDateNumber(now.day()));
+    lcd.setCursor(4,2);
+    lcd.print("WORKING MODE");
+    workingModeValue ? lcd.setCursor(6,3) : lcd.setCursor(5,3);
+    lcd.print(workingMode[workingModeValue]);
+
+    if (rotaryBtnState) {
+      screenExit = true;
+      screenLevel = 1;
+    }
+  }
 }
 
 void runWorkingMode() {
-  short i=0;
-  if (drawScreenOnce) {
-    i=0;
-    indicatorRow = 2;
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("SET WORKING MODE");
-    lcd.setCursor(0,2);
-    lcd.print("Mode: ");
-    lcd.setCursor(6,2);
-    lcd.print(workingMode[i]);
-    drawScreenOnce = false;
-  }
-  if ( rotaryState == 1 ) {
-    Serial.println(i);
-    i++;
-    if (i > 1) {
-      i=0; 
+  byte savedValue = EEPROM.read(eepromWorkingModeAdr);
+  if (savedValue != 0 && savedValue != 1) savedValue = 0;
+  short i = savedValue;
+  indicatorRow = 2;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("SET WORKING MODE");
+  lcd.setCursor(0,2);
+  lcd.print("Mode: ");
+  lcd.setCursor(6,2);
+  lcd.print(workingMode[savedValue]);
+
+  while(!screenExit) {
+    
+    rotaryBtnState = rotary.push();
+    rotaryState = rotary.rotate();
+    
+    if (rotaryState == 1) { // CW
+      i++;
+      if (i > 1) i=0;
     }
-    lcd.setCursor(6,2);
-    lcd.print("                   ");
-    lcd.setCursor(6,2);
-    lcd.print(workingMode[i]);
-    Serial.println(i);
+
+    if (rotaryState == 2) { // CCW
+      i--;
+      if (i < 0) i=1;
+    }
+
+    if (rotaryState == 1 || rotaryState == 2) {
+      lcd.setCursor(6,2);
+      lcd.print("                   ");
+      lcd.setCursor(6,2);
+      lcd.print(workingMode[i]);
+    }
+    
+    if (rotaryBtnState) {
+      savedValue = i;
+      EEPROM.write(eepromWorkingModeAdr, savedValue);
+      screenExit = true;
+      screenLevel = 1;
+    }
   }
 }
 
@@ -166,25 +207,27 @@ void setup() {
   lcd.noBacklight();
 
   rotary.setTrigger(LOW);
+//  rotary.setErrorDelay(0);
 //  rotary.setDebounceDelay(5);
   
   Serial.begin(9600);  
   
-  runMenuSettings();
+  runHomeScreen();
 }
 
 void loop() {
-     
-  rotaryState = rotary.rotate(); // 0 = not turning, 1 = CW, 2 = CCW
-  rotaryBtnState = rotary.pushType(700);
 
-  if ( rotaryBtnState == 1 ) {
-    Serial.println(String("Pressed: ") + "Short");
-  }
+  screenExit = false;
+//  rotaryState = rotary.rotate(); // 0 = not turning, 1 = CW, 2 = CCW
+//  rotaryBtnState = rotary.push();
 
-  if ( rotaryBtnState == 2 ) {
-    Serial.println(String("Pressed: ") + "Long");
-  }
+//  if ( rotaryBtnState == 1 ) {
+////    Serial.println(String("Pressed: ") + "Short");
+//  }
+//
+//  if ( rotaryBtnState == 2 ) {
+////    Serial.println(String("Pressed: ") + "Long");
+//  }
 
   switch(screenLevel) {
     case 0:
